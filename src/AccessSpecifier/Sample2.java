419e1a8ee3e8d941476981599cd7d4ff8fef143d
❤Chinmay❤ <Chinmay@DESKTOP-S4B5J1M.mshome.net>
2023-07-26 10:30:55 +0530

    [26/072023]

--- src/AccessSpecifier/Sample2.java
+++ src/AccessSpecifier/Sample2.java
@@ -4,8 +4,8 @@ public class Sample2
 {
 	public static void main(String[] args)
 	{		
-		Sample1 s1=new Sample1();
-		s1.m1();
-		System.out.println(s1.a);		
+//		Sample1 s1=new Sample1();
+//		s1.m1();
+//		System.out.println(s1.a);		
 	}
 }
--- src/Inheritance/demo.java
+++ src/Inheritance/demo.java
@@ -1,7 +1,7 @@
 package Inheritance;
 
-public class demo  extends demo1, demo2
-{
-	
-
-}
+//public class demo  extends demo1, demo2
+//{
+//	This is exaple of Multiple Inheritance which gives error
+// Multiple inheritance not possible in normal class
+//}
